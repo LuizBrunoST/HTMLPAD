34da6b0a85e9d059932f98a90fd120cdf90e10ed
LUMATECH apps e games <64446877+LuizBrunoST@users.noreply.github.com>
2021-11-28 19:24:31 -0300

    editor offline

--- Web/lib/js/funcLoad.js
+++ Web/lib/js/funcLoad.js
@@ -1,10 +1,23 @@
 window.onload = function() {
+    
 	html = ace.edit("html")
+    html.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true,
+        tabSize: 4
+    })
 	html.setTheme("ace/theme/monokai")
 	html.session.setMode("ace/mode/html")
     html.session.setValue("<p>Olá, mundo!</p>")
 
     css = ace.edit("css")
+    css.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true,
+        tabSize: 4
+    })
 	css.setTheme("ace/theme/monokai")
 	css.session.setMode("ace/mode/css")
     css.session.setValue(
@@ -17,4 +30,10 @@ window.onload = function() {
 	javascript.setTheme("ace/theme/monokai")
 	javascript.session.setMode("ace/mode/javascript")
     javascript.session.setValue("")
+    javascript.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true,
+        tabSize: 4
+    })
 }
