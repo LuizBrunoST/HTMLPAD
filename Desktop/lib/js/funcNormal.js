6ed59fa778b8817021ae069843719a9c9d8b880f
LUMATECH apps e games <64446877+LuizBrunoST@users.noreply.github.com>
2021-11-28 23:54:40 -0300

    munus

--- Desktop/lib/js/funcNormal.js
+++ Desktop/lib/js/funcNormal.js
@@ -2,3 +2,5 @@ htmlpad = "HTMLPAD++"
 versao = "<i>v2.9</i>"
 document.querySelector("title").innerHTML = htmlpad + " " + versao
 document.querySelector("#nomeApp").innerHTML = htmlpad + " " + versao
+
+w3.includeHTML()
