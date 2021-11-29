e8ec89e6429a58d5de8be31830f18bde688eea10
LUMATECH apps e games <64446877+LuizBrunoST@users.noreply.github.com>
2021-11-29 01:29:28 -0300

    loading

--- Desktop/lib/js/funcNormal.js
+++ Desktop/lib/js/funcNormal.js
@@ -1,1 +1,6 @@
 //funcções normais
+
+setTimeout(function () {
+    document.querySelector("#editor-default").style.display='block'
+    document.querySelector("#loading").style.display='none'
+},3000)
